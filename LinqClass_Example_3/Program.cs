f874830cd8325baa7ddef9091ae00f2c1ef689cc
Vladimir <vvsoldatov@rt-dc.ru>
2023-10-14 13:47:23 +0300

    Add lesson

--- LinqClass_Example_3/Program.cs
+++ LinqClass_Example_3/Program.cs
@@ -9,15 +9,18 @@ namespace LinqClass
         static void Main(string[] args)
         {
             int[] arrayInt = { 1, 2, 5, 7, 8, 9, 55, 4, 4, 7 };
-            IEnumerable<int> query = from i in arrayInt select i;
-            foreach(int item in query)
+            IEnumerable<int> query = from i in arrayInt
+                                     where i % 2 == 0
+                                     orderby i descending
+                                     select i;
+            foreach (int item in query)
             {
                 Console.Write($"{item}\t ");
             }
             Console.WriteLine();
             int a, b;
             Console.WriteLine("Enter index,and new value");
-            (a, b) = (Int32.Parse(Console.ReadLine()),Int32.Parse(Console.ReadLine()));
+            (a, b) = (Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()));
             arrayInt[a] = b;
             foreach (int item in query)
             {
