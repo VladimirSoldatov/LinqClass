c86e1aaef55e2d3083e54826dd1ffea15aa9d660
Vladimir <vvsoldatov@rt-dc.ru>
2023-10-14 14:17:24 +0300

    add another lessons

---
+++ LinqClass_Exemple_4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("LinqClass_Exemple_4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LinqClass_Exemple_4")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("61fbe0a7-d7cf-44c0-8f8f-fc0be4166aba")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LinqClass_Exemple_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayInt = { 31, 22, 5, 17, 22, 8, 9, 55, 4, 44, 7 };
+            IEnumerable<IGrouping<int, int>> query =
+                                     from i in arrayInt
+                                     group i % 10 by i;
+            foreach (IGrouping<int, int> item in query)
+            {
+                Console.WriteLine($"{item.Key}\t");
+            }
+            Console.WriteLine();
+            int a, b;
+            Console.WriteLine("Enter index,and new value");
+            (a, b) = (Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()));
+            arrayInt[a] = b;
+            foreach (IGrouping<int, int> item in query)
+            {
+                Console.Write($"{item.Key}\t ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
