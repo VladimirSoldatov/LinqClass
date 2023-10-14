e3fba990ea51ae683b480ca68c7a3a6a58abd41a
Vladimir <vvsoldatov@rt-dc.ru>
2023-10-14 12:48:22 +0300

    Добавьте файлы проекта.

---
+++ LinqClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayInt = { 1, 2, 5, 7, 8, 9, 55, 4, 4, 7 };
+            IEnumerable<int> query = from i in arrayInt select i;
+            foreach(int item in query)
+            {
+                Console.Write($"{item}\t ");
+            }
+            Console.WriteLine();
+            int a, b;
+            Console.WriteLine("Enter index,and new value");
+            (a, b) = (Int32.Parse(Console.ReadLine()),Int32.Parse(Console.ReadLine()));
+            arrayInt[a] = b;
+            foreach (int item in query)
+            {
+                Console.Write($"{item}\t ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
